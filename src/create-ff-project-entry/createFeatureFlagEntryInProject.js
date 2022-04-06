4c0fe78c7bcecf5de4554460d95c1a46035d8519
Basia B <basiab222@gmail.com>
2022-04-06 16:01:11 -0700

    add additional packages

---
+++ src/create-ff-project-entry/createFeatureFlagEntryInProject.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const getAndParsePullRequestDescriptionForFeatureFlag = () => {
+  const { pull_request: pullRequest } = github.context.payload;
+  core.debug(`Pull Request: ${JSON.stringify(pullRequest)}`);
+  if (pullRequest === undefined || pullRequest?.body === undefined) {
+    throw new Error('This action should only be run with Pull Request Events');
+  }
+  return pullRequest.body;
+};
+
+const createFeatureFlagEntryInProject = () => {
+  try {
+    core.debug('Starting PR Description Check for New FF');
+
+    const featureFlagName = getAndParsePullRequestDescriptionForFeatureFlag();
+
+    core.debug(featureFlagName);
+
+    core.info(`Adding to Project, Feature Flag: ${featureFlagName}`);
+  } catch (error) {
+    core.setFailed(error?.message);
+  }
+};
+
+createFeatureFlagEntryInProject()
