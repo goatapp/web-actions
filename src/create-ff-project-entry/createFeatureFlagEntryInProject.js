612e5364bdce9755d114c2d8db621e7d083e8e46
Basia B <basiab222@gmail.com>
2022-04-06 19:27:44 -0700

    log pull request

--- src/create-ff-project-entry/createFeatureFlagEntryInProject.js
+++ src/create-ff-project-entry/createFeatureFlagEntryInProject.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 const getAndParsePullRequestDescriptionForFeatureFlag = () => {
   const { pull_request: pullRequest } = github.context.payload;
   core.debug(`Pull Request: ${JSON.stringify(pullRequest)}`);
-  core.info( JSON.stringify(pullRequest.user.login));
+  core.info( JSON.stringify(pullRequest));
   if (pullRequest === undefined || pullRequest?.body === undefined) {
     throw new Error('This action should only be run with Pull Request Events');
   }
@@ -25,6 +25,7 @@ const createFeatureFlagEntryInProject = () => {
       issue_number: context.issue.number,
       owner: assignee,
       repo: context.repo.repo,
+      title: featureFlag,
       labels: ['Temporary Feature Flag']
     });
 
