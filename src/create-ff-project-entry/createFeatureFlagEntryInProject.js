f2bc117cac20af1cad97ac1b6d78c4173813b059
Basia B <basiab222@gmail.com>
2022-04-06 19:35:36 -0700

    clean up log

--- src/create-ff-project-entry/createFeatureFlagEntryInProject.js
+++ src/create-ff-project-entry/createFeatureFlagEntryInProject.js
@@ -4,7 +4,6 @@ const github = require('@actions/github');
 const getAndParsePullRequestDescriptionForFeatureFlag = () => {
   const { pull_request: pullRequest } = github.context.payload;
   core.debug(`Pull Request: ${JSON.stringify(pullRequest)}`);
-  core.info( JSON.stringify(pullRequest));
   if (pullRequest === undefined || pullRequest?.body === undefined) {
     throw new Error('This action should only be run with Pull Request Events');
   }
