c6a1a4271c9e8f257694508403baa000755863ec
Basia B <basiab222@gmail.com>
2022-04-08 13:49:53 -0700

    search for fields

--- src/create-ff-project-entry/createFeatureFlagEntryInProject.js
+++ src/create-ff-project-entry/createFeatureFlagEntryInProject.js
@@ -102,7 +102,7 @@ const createFeatureFlagEntryInProject = async () => {
       const projectFields = await octokit.graphql(projectFieldsdQuery);
       const dateField = getFieldFromProject('Date Added', projectFields.node.fields.nodes);
       core.info(`FIELDS: ${projectFields.node.fields.nodes}`);
-      core.info(dateField.data.node_id);
+      core.info(JSON.stringify(dateField));
       const newProjectRow = await octokit.graphql(query);
       core.info(`new row attr ${newProjectRow}`);
 
