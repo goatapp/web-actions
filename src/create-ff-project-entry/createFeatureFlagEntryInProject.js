65ea15e4de702bf10e953b673211ec2a83e68719
Basia B <basiab222@gmail.com>
2022-04-08 17:08:15 -0700

    get options

--- src/create-ff-project-entry/createFeatureFlagEntryInProject.js
+++ src/create-ff-project-entry/createFeatureFlagEntryInProject.js
@@ -99,7 +99,7 @@ const createFeatureFlagEntryInProject = async () => {
       core.info(`FIELDS: ${projectFields.node.fields.nodes}`);
       core.info(JSON.stringify(dateField));
       core.info(JSON.stringify(featureAreaField));
-      core.info(`${statusField}`);
+      core.info(`${JSON.stringify(statusField)}`);
 
       const newProjectRow = await octokit.graphql(query);
       const today = (new Date()).toISOString().split('T')[0];
