5863b2971a2b3b3a89f9fc9ae8022561eaa0bb35
Basia B <basiab222@gmail.com>
2022-04-08 17:03:34 -0700

    get options

--- src/create-ff-project-entry/createFeatureFlagEntryInProject.js
+++ src/create-ff-project-entry/createFeatureFlagEntryInProject.js
@@ -106,6 +106,7 @@ const createFeatureFlagEntryInProject = async () => {
 
       const updateDateFieldQuery = buildFieldQuery(JSON.stringify(project.organization.projectNext.id), JSON.stringify(newProjectRow.addProjectNextItem.projectNextItem.id), JSON.stringify(dateField.id), JSON.stringify(today));
       const updateFeatureAreaFieldQuery = buildFieldQuery(JSON.stringify(project.organization.projectNext.id), JSON.stringify(newProjectRow.addProjectNextItem.projectNextItem.id), JSON.stringify(featureAreaField.id));
+      core.info(`Made it to status`);
       const updateStatusQuery = buildFieldQuery(JSON.stringify(project.organization.projectNext.id), JSON.stringify(newProjectRow.addProjectNextItem.projectNextItem.id), JSON.stringify(statusField.id), "f75ad846");
     
 
