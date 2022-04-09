217c701372ce689f6c7d17a8a0dfa8e088e3c330
Basia B <basiab222@gmail.com>
2022-04-08 20:42:27 -0700

    parse feature area from url

--- src/create-ff-project-entry/createFeatureFlagEntryInProject.js
+++ src/create-ff-project-entry/createFeatureFlagEntryInProject.js
@@ -67,7 +67,7 @@ const createFeatureFlagEntryInProject = async () => {
 
     const isFeatureFlagExisting = await octokit.graphql(`{search(
     type:ISSUE, 
-    query: "title: temp_web_enable_use_brand_feed",
+    query: "title:temp_web_enable_use_brand_feed",
     last: 100
   ) {
     repos: edges {
